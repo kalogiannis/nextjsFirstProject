eecf504352ad387ab2767e32625d922bb0d144c6
kalogiannis <85567795+kalogiannis@users.noreply.github.com>
2025-03-23 13:08:32 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter,Lusitana,Caveat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+
+  export const caveat = Caveat({
+    weight: ['400','700'],
+    subsets: ['latin'],
+  });
